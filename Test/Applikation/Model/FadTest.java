f7096cc4b8486104dab225bfa0ef4f16a2a926cf
louisemogensen <louisemogensen2@gmail.com>
2023-04-11 11:29:18 +0200

    Ændringer i OpretFad

---
+++ Test/Applikation/Model/FadTest.java
@@ -0,0 +1,14 @@
+package Applikation.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FadTest {
+
+    @org.junit.jupiter.api.Test
+    void addWhiskey() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeWhiskey() {
+    }
+}
