c117f160213269e78314213f02aa5e07f480e419
louisemogensen <louisemogensen2@gmail.com>
2023-04-13 01:46:14 +0200

    Ændringer i

--- Test/Controller/ControllerTest.java
+++ Test/Controller/ControllerTest.java
@@ -1,7 +1,16 @@
 package Controller;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerTest {
 
+    @Test
+    void createFad() {
+    }
+
+    @Test
+    void createDestillering() {
+    }
 }
